47c930ecff167e891af4ba739f53b27c8f4781e1
AbhinavJawle <jawleabhinav7@gmail.com>
2026-02-23 18:05:27 +0100

    konto erstellen init

--- python.py
+++ python.py
@@ -1,2 +1,10 @@
 import hashlib
 import getpass
+
+kennwort_manager = {}
+
+def konto_erstellen():
+    benutzername = input("Eingabe Benutzername: ")
+    passwort = getpass.getpass("Eingabe Passwort: ")
+    
+
