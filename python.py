3d75e8180ed68a365bd1cd1ea0b1733972e7a5bc
AbhinavJawle <jawleabhinav7@gmail.com>
2026-02-23 18:07:33 +0100

    konto erstellen func

--- python.py
+++ python.py
@@ -6,5 +6,7 @@ kennwort_manager = {}
 def konto_erstellen():
     benutzername = input("Eingabe Benutzername: ")
     passwort = getpass.getpass("Eingabe Passwort: ")
-    
+    hashed_passwort = hashlib.sha256(passwort.encode()).hexdigest()
+    kennwort_manager[benutzername] = hashed_passwort
+    print("Konto erfolgreich erstellt!")
 
