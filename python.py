440cbb1ee8a2d4e212b57ea867f1dafad2317419
AbhinavJawle <jawleabhinav7@gmail.com>
2026-02-23 18:12:22 +0100

    login func

--- python.py
+++ python.py
@@ -10,3 +10,12 @@ def konto_erstellen():
     kennwort_manager[benutzername] = hashed_passwort
     print("Konto erfolgreich erstellt!")
 
+
+def login():
+    benutzername = input("Eingabe Benutzername: ")
+    passwort = getpass.getpass("Eingabe Passwort: ")
+    hashed_passwort = hashlib.sha256(passwort.encode()).hexdigest()
+    if benutzername in kennwort_manager.keys() and kennwort_manager[benutzername] == hashed_passwort:
+        print("Login erfolgreich!")
+    else:
+        print("Falscher Benutzername oder Passwort!")
